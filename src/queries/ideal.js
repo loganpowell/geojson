6bfe6ad1ce2fac2db79fb8e81b0a723d273eb2e1
loganpowell <loganp@tepper.cmu.edu>
2018-05-16 17:53:06 -0400

    data wrangling with DataWeb to make JSON mappable

--- src/queries/ideal.js
+++ src/queries/ideal.js
@@ -33,8 +33,8 @@ FeatureCollection(
 var scope = {
   geo_scope: "county",
   vintage: 2012,
-  fips_codes: [012, 102] or * // <- accept an array, * or just one*
-  lat_lngs: [[59.12341, -32.53432],[...]] // optional -> to Tiger
+  fips_codes: [01012, 01102] or * // <- accept an array, * or just one*
+  coordinates: [{lat,lng}...coordinates] // optional -> to Tiger
 }
 var level = {
   geo_level: "tract",
@@ -54,6 +54,60 @@ let makeMappable = (arg) => {
   }
 	return mappable // returns the args packed into an array
 }
+// to deal with the nested argument
+let testFn6 = (...variables) => {
+    let src = variables.map(each => ({
+		source: each.source,
+		data: each.data.map(d => ({ item: d }))
+    }))
+	return src
+}
+testFn6(...variables) // <-- works
+
+// result: JSON.stringify(testFn6(...arg1))
+
+[
+	{
+		"source": "sf1",
+		"data": [
+			{
+				"item": "P028E001"
+			},
+			{
+				"item": "P028E002"
+			},
+			{
+				"item": "P028E005"
+			}
+		]
+	},
+	{
+		"source": "cbp",
+		"data": [
+			{
+				"item": "ESTAB"
+			}
+		]
+	}
+]
+
+
+// fetching and Promise resolution
+let response = fetch("https://api.census.gov/data/2016/acs/acs5/examples.json")
+	.then(r => r.json())
+// need cors to use Github as an endpoint... will need meaningful
+// file names (use same name as dataweb) e.g., "2010_Counties.json"
+let response = fetch("https://cors-anywhere.herokuapp.com/https://github.com/loganpowell/geojson/raw/master/src/data/test.geojson").then(res => {
+	res.json()
+})
+// then use the resulting promise
+response.then(r => {
+  // you have to use the value in here... potentially in a block-scoped
+  // variable that can be used elsewhere :(
+})
+
+
+
 
 getProductsByGeoLevel(vintage: vintage){
   geo_levels {
